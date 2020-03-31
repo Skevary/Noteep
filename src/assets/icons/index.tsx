549a23c031d2c7a4b62845a4e263aef9fd8f1191
Alexander Raievskyi <araievskyi@gmail.com>
2020-03-31 15:47:32 +0300

    new(assets): add svg icons resources

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,82 @@
+import bar from './bar.svg';
+import bookmarkAlt from './bookmark-alt.svg';
+import bookmarks from './bookmarks.svg';
+import boxAlt from './box-alt.svg';
+import box from './box.svg';
+import cancel from './cancel.svg';
+import circleChecked from './circle-checked.svg';
+import circlePlus from './circle-plus.svg';
+import clock from './clock.svg';
+import code from './code.svg';
+import collapse from './collapse.svg';
+import contrast from './contrast.svg';
+import cross from './cross.svg';
+import deleteBin from './delete-bin.svg';
+import file from './file.svg';
+import folder from './folder.svg';
+import healthData from './health-data.svg';
+import lightOn from './light-on.svg';
+import light from './light.svg';
+import lightbulbSolid from './lightbulb-solid.svg';
+import lock from './lock.svg';
+import logo from './logo.svg';
+import openedFolder from './opened-folder.svg';
+import padlock from './padlock.svg';
+import paintPlate from './paint-plate.svg';
+import pen from './pen.svg';
+import pencil from './pencil.svg';
+import picture from './picture.svg';
+import pin from './pin.svg';
+import redo from './redo.svg';
+import saveClose from './save-close.svg';
+import search from './search.svg';
+import settings from './settings.svg';
+import tickBox from './tick-box.svg';
+import trash from './trash.svg';
+import undo from './undo.svg';
+import unpin from './unpin.svg';
+import update from './update.svg';
+import viewHeadline from './view-headline.svg';
+
+
+export default {
+    bar,
+    bookmarkAlt,
+    bookmarks,
+    boxAlt,
+    box,
+    cancel,
+    circleChecked,
+    circlePlus,
+    clock,
+    code,
+    collapse,
+    contrast,
+    cross,
+    deleteBin,
+    file,
+    folder,
+    healthData,
+    lightOn,
+    light,
+    lightbulbSolid,
+    lock,
+    logo,
+    openedFolder,
+    padlock,
+    paintPlate,
+    pen,
+    pencil,
+    picture,
+    pin,
+    redo,
+    saveClose,
+    search,
+    settings,
+    tickBox,
+    trash,
+    undo,
+    unpin,
+    update,
+    viewHeadline
+};
