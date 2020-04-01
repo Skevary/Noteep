02e49e5c919c975357cd8e66d7bb74279ffc6bbe
Alexander Raievskyi <araievskyi@gmail.com>
2020-04-01 19:49:18 +0300

    update(sidebar): update styles

--- src/app/sidebar/footer.tsx
+++ src/app/sidebar/footer.tsx
@@ -7,24 +7,25 @@ export default function Footer(props: Props) {
     const goTo = (url: string, target?: string) => window.open(url, target);
 
     return (<footer>
-        <button
-            className="btn-row"
+        <div
+            className="item"
             title={'Author github account'}
             onClick={() => goTo('https://github.com/Skevary', '_blank')}>
-            <div className={'wrapper'}>
-                <SvgIcon icon={'Github'}>{'Github'}</SvgIcon>
-                <span className="trim-text">{'Github'}</span>
-            </div>
-        </button>
+        <span className="icon-btn">
+                <SvgIcon icon={'Github'} />
+        </span>
+            <span className="text trim-text">{'Github'}</span>
+        </div>
 
-        <button
-            className="btn-row"
+        <div
+            className="item"
             title={'Author gmail account'}
             onClick={() => goTo('mailto:araievskyi@gmail.com')}>
-            <div className={'wrapper'}>
-                <SvgIcon icon={'Gmail'} style={{width: '1.75em', height: '1.75em'}}/>
-                <span className="trim-text">{'Gmail'}</span>
-            </div>
-        </button>
+        <span className="icon-btn">
+                <SvgIcon icon={'Gmail'}  style={{width: '1.75em', height: '1.75em'}}/>
+        </span>
+            <span className="text trim-text">{'Gmail'}</span>
+        </div>
+
     </footer>)
 }
